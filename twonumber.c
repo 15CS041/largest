ffbe35b3f50a17ba54c9065c87551b516cd1e01a
15CS041 <rajaudhayaap1998@gmail.com>
2018-03-13 16:13:51 +0530

    Create twonumber.c

---
+++ twonumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+inti,j,k,n,sum=0;
+printf("enter the value");
+scanf("%d%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("Enter the value i");
+}
+for(j=1;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("enter the sum %d",k);
+return 0;
+}
